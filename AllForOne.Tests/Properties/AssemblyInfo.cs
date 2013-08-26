220662f0023a47fb124f60ab09c06a250efc034a
Jonathan Holford <jonathan@cavepug.com>
2013-08-26 14:50:58 +0100

    Restoring versioning back to local AssemblyInfo file, to test out TeamCity Assembly Versioning feature.

--- AllForOne.Tests/Properties/AssemblyInfo.cs
+++ AllForOne.Tests/Properties/AssemblyInfo.cs
@@ -3,3 +3,6 @@
 [assembly: AssemblyTitle("Holf.AllForOne.Tests")]
 [assembly: AssemblyProduct("Holf.AllForOne.Tests")]
 
+
+[assembly: AssemblyVersion("9.9.9.9")]
+[assembly: AssemblyFileVersion("9.9.9.9")]
